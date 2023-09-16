d8769f338ee52330dbc3c67b3fac6a9a344b0545
KuzmynMaksym <144415346+KuzmynMaksym@users.noreply.github.com>
2023-09-16 14:50:39 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    //#a, b = map(int, input())
+	double a, b;
+	cout << "Enter a:";
+	cin >> a;
+	// a = int(input())
+	// b = int(input())
+	b = 4 * a;
+	// print((a+b) /2)
+	cout << "Perimeter:" << b << endl;
+	return 0;
+}
